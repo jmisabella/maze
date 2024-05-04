a8bd4692e3293ec90976c0e82c194560de6504c7
Jeffrey <jeff.isabella@gmail.com>
2024-05-04 14:29:10 -0400

    intermittent commit
    intermittent commit

--- src/main/scala/classes/Cell.scala
+++ src/main/scala/classes/Cell.scala
@@ -1,3 +1,31 @@
 package maze.classes
 
-case class Cell(row: Int, column: Int)
+import maze.classes.Direction._
+
+case class Coordinates(x: Int, y: Int)
+
+case class Neighbors(
+  north: Option[Coordinates] = None,
+  east: Option[Coordinates] = None,
+  south: Option[Coordinates] = None,
+  west: Option[Coordinates] = None
+)
+
+case class Cell(
+  coordinates: Coordinates, 
+  neighbors: Neighbors = Neighbors(), 
+  linked: Set[Coordinates] = Set() 
+) {
+  def link(coordinates: Option[Coordinates]): Cell = coordinates match {
+    case None => this
+    case Some(c) => this.copy(linked = this.linked + c)
+  }
+  def unlink(coordinates: Option[Coordinates]): Cell = coordinates match {
+    case None => this
+    case Some(c) => this.copy(linked = this.linked - c)
+  }
+  def isLinked(coordinates: Option[Coordinates]): Boolean = coordinates match {
+    case None => false
+    case Some(c) => linked.contains(c)
+  }
+}
--- src/main/scala/classes/Grid.scala
+++ src/main/scala/classes/Grid.scala
@@ -1,3 +1,18 @@
 package maze.classes
 
-case class Grid()
+import maze.classes.Cell
+
+case class Grid(
+  rows: Int, 
+  columns: Int, 
+  cells: Array[Array[Cell]] = Array()) {
+
+  def links(cell: Cell): Seq[Cell] = (for (c <- cell.linked) yield cells(c.x)(c.y)).toSeq
+  def linked(cell1: Cell, cell2: Cell): Boolean = cell1.isLinked(Some(cell2.coordinates))
+  
+  def prepareGrid(): Grid = this.copy(cells = Array.ofDim[Cell](this.rows, this.columns))
+
+  // def prepareCells(): Grid = this.copy(cells = ) 
+
+}
+
