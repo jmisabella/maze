3d0a5a834ad459c7a6f0ffaf70714c0883453a99
Jeffrey <jeff.isabella@gmail.com>
2024-05-22 22:07:27 -0400

    seems to have fixed issue of isolated cells in Sidewinder maze
    seems to have fixed issue of isolated cells in Sidewinder maze

--- src/main/scala/behaviors/builders/Generator.scala
+++ src/main/scala/behaviors/builders/Generator.scala
@@ -14,27 +14,8 @@ trait Generator {
   def generate(grid: Grid): Grid
   def generate(x: Int, y: Int): Grid = generate(Grid(x, y))
 
-    // given grid, randomly removes a wall from any unreachable cells 
+  // given grid, randomly removes a wall from any unreachable cells 
   protected def linkUnreachables(grid: Grid): Grid = {
-    // def deisolate(grid: Grid, startX: Int, startY: Int): Grid = {
-    //   var nextGrid: Grid = grid
-    //   var dist = distance.distances(nextGrid, startX, startY)
-    //   var unreachables: Seq[Cell] = nextGrid.flatten.filter(c => !dist.keySet.contains(c.coords))
-    //   while (!unreachables.isEmpty) {
-    //     val unreached: Cell = unreachables.head
-    //     val unlinkedNeighbors: Seq[Coordinates] = unreached.availableNeighbors().filter(c => !unreached.linked.contains(c)).toSeq
-    //     val (i, nextSeed) = nextGrid.randomInt(unlinkedNeighbors.length)
-    //     val cell: Cell = nextGrid.get(unlinkedNeighbors(i))
-    //     val linkedCells: Seq[Cell] = linker.link(Seq(unreached, cell))
-    //     nextGrid = nextGrid.copy(seed = nextSeed)
-    //     nextGrid = nextGrid.set(linkedCells.head)
-    //     nextGrid = nextGrid.set(linkedCells.reverse.head)
-    //     dist = distance.distances(nextGrid, 0, 0)
-    //     unreachables = nextGrid.flatten().filter(c => !dist.keySet.contains(c.coords))
-    //   }
-    //   nextGrid
-    // }
-
     def deisolate(grid: Grid): Grid = {
       var nextGrid: Grid = grid
       var unreachables: Seq[Cell] = nextGrid.flatten.filter(c => !nextGrid.reachable(0, 0, c.coords.x, c.coords.y))
@@ -45,14 +26,15 @@ trait Generator {
         val cell: Cell = nextGrid.get(unlinkedNeighbors(i))
         val linkedCells: Seq[Cell] = linker.link(Seq(unreached, cell))
         nextGrid = nextGrid.copy(seed = nextSeed)
-        nextGrid = nextGrid.set(linkedCells.head)
-        nextGrid = nextGrid.set(linkedCells.reverse.head)
+        for (linked <- linkedCells) {
+          nextGrid = nextGrid.set(linked)
+        }
         unreachables = nextGrid.flatten.filter(c => !nextGrid.reachable(0, 0, c.coords.x, c.coords.y))
       }
       nextGrid
     }
-    // deisolate(deisolate(grid, 0, 0), grid.rows - 1, grid.columns - 1)
-    deisolate(deisolate(grid))
+    // deisolate(deisolate(grid))
+    deisolate(grid)
   }
 
 }
