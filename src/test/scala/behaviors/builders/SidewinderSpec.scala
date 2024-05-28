c9c6c4242d382254d559b93812a4df7f0460b3eb
Jeffrey <jeff.isabella@gmail.com>
2024-05-27 23:03:06 -0400

    intermittent commit
    intermittent commit

--- src/test/scala/behaviors/builders/SidewinderSpec.scala
+++ src/test/scala/behaviors/builders/SidewinderSpec.scala
@@ -54,39 +54,40 @@ class SidewinderSpec extends AnyFlatSpec with GivenWhenThen {
   }
 
 
-  // it should "generate a 25x25 maze using Sidewinder and print it to screen" in {
-  //   Given("25x25 grid")
-  //   val grid = Grid(25, 25)
-  //   When("generating maze using Sidewinder")
-  //   val generated: Grid = module.generate(grid)
-  //   Then("generated maze should have height of 25 cells")
-  //   generated.rows should be (25)
-  //   generated.cells.length should be (25)
-  //   Then("generated maze should have width of 5 cells")
-  //   generated.columns should be (25)
-  //   generated.cells.count(c => c.length == 25) should be (25)
-  //   When("printing the maze")
-  //   Then("the maze should be printed to screen")
-  //   println("25x25 Sidewinder")
-  //   println(generated.toString())
-  // }
+  it should "generate a 25x25 maze using Sidewinder and print it to screen" in {
+    Given("25x25 grid")
+    val grid = Grid(25, 25)
+    When("generating maze using Sidewinder")
+    val generated: Grid = module.generate(grid)
+    Then("generated maze should have height of 25 cells")
+    generated.rows should be (25)
+    generated.cells.length should be (25)
+    Then("generated maze should have width of 5 cells")
+    generated.columns should be (25)
+    generated.cells.count(c => c.length == 25) should be (25)
+    When("printing the maze")
+    Then("the maze should be printed to screen")
+    println("25x25 Sidewinder")
+    println(generated.toString())
+  }
 
-  // it should "generate a 35x35 maze using Sidewinder and print it to screen" in {
-  //   Given("35x35 grid")
-  //   val grid = Grid(35, 35)
-  //   When("generating maze using Sidewinder")
-  //   val generated: Grid = module.generate(grid)
-  //   Then("generated maze should have height of 35 cells")
-  //   generated.rows should be (35)
-  //   generated.cells.length should be (35)
-  //   Then("generated maze should have width of 5 cells")
-  //   generated.columns should be (35)
-  //   generated.cells.count(c => c.length == 35) should be (35)
-  //   When("printing the maze")
-  //   Then("the maze should be printed to screen")
-  //   println("35x35 Sidewinder")
-  //   println(generated.toString())
-  // }
+  it should "generate a 35x35 maze using Sidewinder and print it to screen" in {
+    Given("35x35 grid")
+    val grid = Grid(35, 35)
+    When("generating maze using Sidewinder")
+    val generated: Grid = module.generate(grid)
+    Then("generated maze should have height of 35 cells")
+    generated.rows should be (35)
+    generated.cells.length should be (35)
+    Then("generated maze should have width of 5 cells")
+    generated.columns should be (35)
+    generated.cells.count(c => c.length == 35) should be (35)
+    When("printing the maze")
+    Then("the maze should be printed to screen")
+    println("35x35 Sidewinder")
+    println(generated.toString())
+    println(module.distance.showPathTo(generated, 0, 34, 34, 0))
+  }
 
   it should "generate a maze in which each cell is accessible from the upper-left corner cell" in {
     When("generating 5x5 maze using Sidewinder")
