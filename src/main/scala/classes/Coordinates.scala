368e29b74cd3932bd7b71a776c519e29e1619323
Jeffrey <jeff.isabella@gmail.com>
2024-07-03 20:31:08 -0400

    start of heavy reorganization
    cell structure has distance, isStart, isGoal, onSolutionPath

--- src/main/scala/classes/Coordinates.scala
+++ src/main/scala/classes/Coordinates.scala
@@ -1,11 +1,36 @@
 package maze.classes
 
-import play.api.libs.json.{ Json, Format }
+import play.api.libs.json.{ Json, Format, JsSuccess }
+import com.fasterxml.jackson.core.JsonParseException
 
+case class SerializedCoordinates(x: String, y: String) {
+  override def toString(): String = (Json.obj("x" -> x, "y" -> y)).toString()
+}
+object SerializedCoordinates {
+  implicit val format: Format[SerializedCoordinates] = Json.format[SerializedCoordinates]
+}
 case class Coordinates(x: Int, y: Int) {
   override def toString(): String = (Json.obj("x" -> x, "y" -> y)).toString()
 }
 object Coordinates {
   implicit def ordering [A <: Coordinates]: Ordering[A] = Ordering.by(c => (c.x, c.y))
   implicit val format: Format[Coordinates] = Json.format[Coordinates]
+  
+  def apply(coords: SerializedCoordinates): Coordinates = Coordinates(coords.x.toInt, coords.y.toInt)
+
+  def apply(json: String): Coordinates = try {
+    Json.parse(json).validate[Coordinates] match {
+      case JsSuccess(coords, _) => coords 
+      case e => throw new IllegalArgumentException("Error occurred deserializing json into Coordinates") 
+    }
+  } catch {
+    case _: Exception => try {
+      Json.parse(json).validate[SerializedCoordinates] match {
+        case JsSuccess(coords, _) => Coordinates(coords) 
+        case e => throw new IllegalArgumentException("Error occurred deserializing json into SerializedCoordinates") 
+      } 
+    } catch {
+      case e: JsonParseException => throw e
+    }
+  }
 }
