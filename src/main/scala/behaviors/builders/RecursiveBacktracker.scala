beb386c7382612c53e15567e49aad591c58cd091
Jeffrey <jeff.isabella@gmail.com>
2024-08-23 18:29:26 -0400

    0.0.22: reworked Recursive Backtracker to use a tail-recursive solution
    0.0.22: reworked Recursive Backtracker to use a tail-recursive solution

--- src/main/scala/behaviors/builders/RecursiveBacktracker.scala
+++ src/main/scala/behaviors/builders/RecursiveBacktracker.scala
@@ -12,32 +12,28 @@ trait RecursiveBacktracker extends Generator {
   val linker: LINKAGE
   
   override def generate(grid: Grid): Grid = {
-    var nextGrid: Grid = grid
-    val visited: ListBuffer[Coordinates] = new ListBuffer[Coordinates]()
-    val (randomIndex1, seed1): (Int, RNG) = nextGrid.randomInt(nextGrid.size())
-    nextGrid = nextGrid.copy(seed = seed1)
-    var current: Cell = nextGrid.flatten()(randomIndex1)
-    visited += current.coords
-    val stack = Stack[Cell](current)
-    while (stack.nonEmpty) {
-      current = nextGrid.get(stack.top.coords)
-      val unvisitedNeighbors: Seq[Cell] = nextGrid.unlinkedNeighbors(current).filter(c => !visited.contains(c.coords))
-      if (unvisitedNeighbors.nonEmpty) {
-        val (randomIndex2, seed2): (Int, RNG) = nextGrid.randomInt(unvisitedNeighbors.length)
-        nextGrid = nextGrid.copy(seed = seed2)
-        var neighbor: Cell = unvisitedNeighbors(randomIndex2)
-        println("LINKING " + current.coords + " TO " + neighbor.coords)
-        // current = current.copy(linked = current.linked ++ Set(neighbor.coords))
-        // neighbor = neighbor.copy(linked = neighbor.linked ++ Set(current.coords))
-        // nextGrid = nextGrid.set(current).set(neighbor)
-        nextGrid = linker.link(current, neighbor, nextGrid)
-        visited += neighbor.coords
-        stack.push(neighbor) 
-      } else {
-        // backtrack if no unvisited neighbors
-        stack.pop() // go backwards 1 cell by popping last visited cell from the stack
-      }
-    }    
-    nextGrid
+    @annotation.tailrec
+    def generateMaze(grid: Grid, stack: List[Cell], visited: Set[Coordinates]): Grid = stack match {
+      case Nil =>
+        grid // 
+      case current :: rest =>
+        var nextGrid: Grid = grid 
+        val currentCell: Cell = nextGrid.get(current.coords)
+        val unvisitedNeighbors: Seq[Cell] = nextGrid.unlinkedNeighbors(current).filter(c => !visited.contains(c.coords))
+        if (unvisitedNeighbors.nonEmpty) {
+          val (randomIndex, seed): (Int, RNG) = nextGrid.randomInt(unvisitedNeighbors.length)
+          nextGrid = nextGrid.copy(seed = seed)
+          val neighbor: Cell = unvisitedNeighbors(randomIndex)
+          nextGrid = linker.link(currentCell, neighbor, nextGrid)
+          generateMaze(nextGrid, neighbor :: stack, visited + neighbor.coords)
+        } else {
+          // backtrack if no unvisited neighbors
+          generateMaze(grid, rest, visited)
+        }
+    }
+    // initialize the stack and visited set with randomly selected first cell
+    val (randomIndex, seed) = grid.randomInt(grid.size())
+    val initialCell: Cell = grid.flatten()(randomIndex)
+    generateMaze(grid.copy(seed = seed), List(initialCell), Set(initialCell.coords))
   }
 }
