e43e0ee3dead2373b868e4ddec51139f3b5890e5
Jeffrey <jeff.isabella@gmail.com>
2024-09-09 19:22:23 -0400

    attempt for Generator.generator generate maze Grid without type params
    attempt for Generator.generator generate maze Grid without type params

--- src/main/scala/behaviors/builders/Generator.scala
+++ src/main/scala/behaviors/builders/Generator.scala
@@ -1,7 +1,5 @@
 package maze.behaviors.builders
 
-// import maze.classes.{ Cell, Grid, Coordinates, MazeRequest, Algorithm }
-// import maze.behaviors.{ Linkage, Distance }
 import maze.classes.{ Coordinates, MazeRequest, Algorithm }
 import maze.behaviors.{ Linkage, Distance, Cell, Grid, Neighbors }
 import maze.behaviors.builders.{ BinaryTree, Sidewinder, AldousBroder, Wilsons, HuntAndKill }
@@ -25,64 +23,82 @@ trait Generator[N <: Neighbors, C <: Cell, G <: Grid[C]] {
 }
 
 object Generator {
-  def generate[A, N <: Neighbors, C <: Cell, G <: Grid[C]](request: MazeRequest)(implicit generator: A <:< Generator[N, C, G], ct: ClassTag[C]): G = {
-    // case object binaryTree extends BinaryTree {
-    //   case object _linkage extends Linkage[SquareNeighbors, SquareCell, SquareGrid]
-    //   override type LINKAGE = Linkage[SquareNeighbors, SquareCell, SquareGrid]
-    //   override val linker = _linkage
-    //   case object _distance extends Distance[SquareNeighbors, SquareCell, SquareGrid]
-    //   override type DISTANCE = Distance[SquareNeighbors, SquareCell, SquareGrid]
-    //   override val distance = _distance
-    // }
-    // case object sidewinder extends Sidewinder {
-    //   case object _linkage extends Linkage[SquareNeighbors, SquareCell, SquareGrid]
-    //   override type LINKAGE = Linkage[SquareNeighbors, SquareCell, SquareGrid]
-    //   override val linker = _linkage
-    //   case object _distance extends Distance[SquareNeighbors, SquareCell, SquareGrid]
-    //   override type DISTANCE = Distance[SquareNeighbors, SquareCell, SquareGrid]
-    //   override val distance = _distance
-    // }
-    case object aldousBroder extends AldousBroder[N, C, G] {
-      case object _linkage extends Linkage[N, C, G]
-      override type LINKAGE = Linkage[N, C, G]
-      override val linker = _linkage
-      case object _distance extends Distance[N, C, G]
-      override type DISTANCE = Distance[N, C, G]
-      override val distance = _distance
+  def generate(request: MazeRequest): Grid[_] = {
+    val generator = (request.mazeType, request.algorithm) match {
+
+      case (Square, Algorithm.BinaryTree) => {
+        // BinaryTree algorithm only eligible for Square MazeType
+        case object binaryTree extends BinaryTree[SquareNeighbors, SquareCell, SquareGrid] {
+          case object _linkage extends Linkage[SquareNeighbors, SquareCell, SquareGrid]
+          override type LINKAGE = Linkage[SquareNeighbors, SquareCell, SquareGrid]
+          override val linker = _linkage
+          case object _distance extends Distance[SquareNeighbors, SquareCell, SquareGrid]
+          override type DISTANCE = Distance[SquareNeighbors, SquareCell, SquareGrid]
+          override val distance = _distance
+        }
+        binaryTree
+      }
+      case (Square, Algorithm.Sidewinder) => {
+        // Sidewinder algorithm only eligible for Square MazeType
+        case object sidewinder extends Sidewinder[SquareNeighbors, SquareCell, SquareGrid] {
+          case object _linkage extends Linkage[SquareNeighbors, SquareCell, SquareGrid]
+          override type LINKAGE = Linkage[SquareNeighbors, SquareCell, SquareGrid]
+          override val linker = _linkage
+          case object _distance extends Distance[SquareNeighbors, SquareCell, SquareGrid]
+          override type DISTANCE = Distance[SquareNeighbors, SquareCell, SquareGrid]
+          override val distance = _distance
+        }
+        sidewinder
+      }
+      case (Square, Algorithm.AldousBroder) => {
+        case object aldousBroder extends AldousBroder[SquareNeighbors, SquareCell, SquareGrid] {
+          case object _linkage extends Linkage[SquareNeighbors, SquareCell, SquareGrid]
+          override type LINKAGE = Linkage[SquareNeighbors, SquareCell, SquareGrid]
+          override val linker = _linkage
+          case object _distance extends Distance[SquareNeighbors, SquareCell, SquareGrid]
+          override type DISTANCE = Distance[SquareNeighbors, SquareCell, SquareGrid]
+          override val distance = _distance
+        }
+        aldousBroder
+      }
+      case (Square, Algorithm.Wilsons) => {
+        case object wilsons extends Wilsons[SquareNeighbors, SquareCell, SquareGrid] {
+          case object _linkage extends Linkage[SquareNeighbors, SquareCell, SquareGrid]
+          override type LINKAGE = Linkage[SquareNeighbors, SquareCell, SquareGrid]
+          override val linker = _linkage
+          case object _distance extends Distance[SquareNeighbors, SquareCell, SquareGrid]
+          override type DISTANCE = Distance[SquareNeighbors, SquareCell, SquareGrid]
+          override val distance = _distance
+        }
+        wilsons
+      }
+      case (Square, Algorithm.HuntAndKill) => {
+        case object huntAndKill extends HuntAndKill[SquareNeighbors, SquareCell, SquareGrid] {
+          case object _linkage extends Linkage[SquareNeighbors, SquareCell, SquareGrid]
+          override type LINKAGE = Linkage[SquareNeighbors, SquareCell, SquareGrid]
+          override val linker = _linkage
+          case object _distance extends Distance[SquareNeighbors, SquareCell, SquareGrid]
+          override type DISTANCE = Distance[SquareNeighbors, SquareCell, SquareGrid]
+          override val distance = _distance
+        }
+        huntAndKill
+      }
+      case (Square, Algorithm.RecursiveBacktracker) => {
+        case object recursiveBacktracker extends RecursiveBacktracker[SquareNeighbors, SquareCell, SquareGrid] {
+          case object _linkage extends Linkage[SquareNeighbors, SquareCell, SquareGrid]
+          override type LINKAGE = Linkage[SquareNeighbors, SquareCell, SquareGrid]
+          override val linker = _linkage
+          case object _distance extends Distance[SquareNeighbors, SquareCell, SquareGrid]
+          override type DISTANCE = Distance[SquareNeighbors, SquareCell, SquareGrid]
+          override val distance = _distance
+        }
+        recursiveBacktracker
+      }
+      case (t, Algorithm.BinaryTree) => throw new IllegalArgumentException(s"BinaryTree algorithm only eligible for Square maze type. It is not eligible for [$t]")
+      case (t, Algorithm.Sidewinder) => throw new IllegalArgumentException(s"Sidewinder algorithm only eligible for Square maze type. It is not eligible for [$t]")
+      case (t, _) => throw new IllegalArgumentException(s"Unexpected maze type [$t]")
+      case (_, a) => throw new IllegalArgumentException(s"Unexpected algorithm [$a]")
     }
-    case object wilsons extends Wilsons[N, C, G] {
-      case object _linkage extends Linkage[N, C, G]
-      override type LINKAGE = Linkage[N, C, G]
-      override val linker = _linkage
-      case object _distance extends Distance[N, C, G]
-      override type DISTANCE = Distance[N, C, G]
-      override val distance = _distance
-    }
-    case object huntAndKill extends HuntAndKill[N, C, G] {
-      case object _linkage extends Linkage[N, C, G]
-      override type LINKAGE = Linkage[N, C, G]
-      override val linker = _linkage
-      case object _distance extends Distance[N, C, G]
-      override type DISTANCE = Distance[N, C, G]
-      override val distance = _distance
-    }
-    case object recursiveBacktracker extends RecursiveBacktracker[N, C, G] {
-      case object _linkage extends Linkage[N, C, G]
-      override type LINKAGE = Linkage[N, C, G]
-      override val linker = _linkage
-      case object _distance extends Distance[N, C, G]
-      override type DISTANCE = Distance[N, C, G]
-      override val distance = _distance
-    }
-    val generator: Generator[N, C, G] = (request.algorithm match {
-      // case Algorithm.BinaryTree => binaryTree
-      // case Algorithm.Sidewinder => sidewinder
-      case Algorithm.AldousBroder => aldousBroder 
-      case Algorithm.Wilsons => wilsons
-      case Algorithm.HuntAndKill => huntAndKill 
-      case Algorithm.RecursiveBacktracker => recursiveBacktracker
-      case a => throw new IllegalArgumentException(s"Unexpected algorithm [$a]")
-    }).asInstanceOf[Generator[N, C, G]]
     generator.distance.pathTo( 
       generator.distance.distances(
         generator.generate(request.mazeType, request.width, request.height, request.start, request.goal)
